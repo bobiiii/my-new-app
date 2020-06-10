3b1c1dbc6a5e5db5d308b33c9ca523e8fa7a0878
bobiiii <52751757+bobiiii@users.noreply.github.com>
2020-06-10 09:13:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import './App.css';
 
 
 function App() {
   return (
-    <div > hello world From babar 
+    <div className="App">
+      <header className="App-header"> hello world From babar 
       Asignment no 2
-      
+      </header>
     </div>
   );
 }
